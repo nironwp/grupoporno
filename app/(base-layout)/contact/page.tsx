8d82fc66537d98fea8c3752154c90f2e64fe41c3
Zeix <zeitherich@proton.me>
2024-03-23 21:37:34 -0300

    g

--- app/(base-layout)/contact/page.tsx
+++ app/(base-layout)/contact/page.tsx
@@ -91,7 +91,7 @@ export default function Home() {
         <div className="relative z-0 col-span-2">
           <textarea onChange={(e) => {
             setMessage(e.target.value)
-          }} name="message" rows="5" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" "></textarea>
+          }} name="message" rows={5} className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" "></textarea>
           <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Sua Mensagem</label>
         </div>
       </div>
